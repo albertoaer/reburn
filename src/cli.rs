0b376c49d185487c19b96a283ea98e251c789855
Alberto Elorza <albertoelorza2002@gmail.com>
2022-12-06 20:51:59 +0100

    find command out from command line arguments

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 
+use crate::parsing::interpreter::get_interpreter;
+
 #[derive(Parser, Debug)]
 pub struct Cli {
 	#[arg(help = "Changes to watch")]
@@ -10,4 +12,20 @@ pub struct Cli {
 
 	#[arg(last = true, help = "Command to run")]
 	pub cmd: Option<Vec<String>>
+}
+
+impl Cli {
+	pub fn command(&self) -> Result<Vec<String>, String> {
+		match (&self.cmd, &self.script) {
+			(Some(_), Some(_)) | (None, None) => Err("Expected either a script or a command".to_string()),
+			(Some(v), None) => Ok(v.clone()),
+			(None, Some(script)) => match get_interpreter(script) {
+				Ok(mut v) => {
+					v.push(script.clone());
+					Ok(v)
+				},
+				Err(e) => Err(e.to_string()),
+			}
+		}
+	}
 }

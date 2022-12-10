9efc9f3699d78f29a81aee5f404b2980760ecc88
Alberto Elorza <albertoelorza2002@gmail.com>
2022-12-10 18:50:34 +0100

    simplify watcher code

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,6 +1,6 @@
 use std::{sync::mpsc::{channel, Receiver}, path::{Path, PathBuf}};
 
-use notify::{Watcher, recommended_watcher, RecommendedWatcher, RecursiveMode};
+use notify::{Watcher, recommended_watcher, RecommendedWatcher, RecursiveMode, Result};
 
 #[derive(Debug)]
 pub struct WatchingChannel<T> {
@@ -9,16 +9,14 @@ pub struct WatchingChannel<T> {
 }
 
 impl WatchingChannel<Vec<PathBuf>> {
-	pub fn try_new<P: AsRef<Path>, I: Iterator<Item = (P, RecursiveMode)>>(targets: I) -> Result<Self, notify::Error> {
+	pub fn try_new<P: AsRef<Path>, I: Iterator<Item = (P, RecursiveMode)>>(targets: I) -> Result<Self> {
 		let (sender, receiver) = channel::<Vec<PathBuf>>();
-		let mut watcher = recommended_watcher(
-			move |res: Result<notify::Event, notify::Error>| match res {
-				Ok(event) => {
-					sender.send(event.paths).ok();
-				},
-				Err(e) => panic!("Watch error: {:?}", e),
-			}
-		)?;
+		let mut watcher = recommended_watcher(move |res: Result<notify::Event>| match res {
+			Ok(event) => {
+				sender.send(event.paths).ok();
+			},
+			Err(e) => panic!("Watch error: {:?}", e),
+		})?;
 		for (path, recursive) in targets {
 			watcher.watch(path.as_ref(), recursive)?;
 		}
@@ -30,9 +28,6 @@ impl<T> Iterator for WatchingChannel<T> {
 	type Item = T;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		match self.receiver.recv() {
-			Ok(n) => Some(n),
-			Err(_) => None
-		}
+		self.receiver.recv().ok()
 	}
 }

9bb56d048e18cf4d972072a39ae550a4c23c4cae
Alberto Elorza <albertoelorza2002@gmail.com>
2022-12-11 13:49:59 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use clap::Parser;
-use notify::RecursiveMode;
-
 mod exec;
 mod cli;
 mod watcher;
 mod parsing;
+mod walk;
+
+use parsing::selector::parse_selector;
+
+fn aux_to_str<E : ToString>(e: E) -> String {
+  e.to_string()
+}
 
 fn main() -> Result<(), String>{
-  let args = cli::Cli::parse().command()?;
-  let w = watcher::WatchingChannel::try_new([(".", RecursiveMode::Recursive)]
-    .into_iter()).unwrap();
-  let update = || exec::run_command(args.as_slice())
-    .map_err(|e| e.to_string());
+  let parsed_args = cli::Cli::new();
+
+  let command = parsed_args.command()?;
+  let update = || exec::run_command(command.as_slice()).map_err(aux_to_str);
   let mut active = update()?;
-  for _ in w {
+  
+  let targets = walk::matches(parse_selector(&parsed_args.watch)?)?;
+
+  for _ in watcher::WatchingChannel::try_new(targets).map_err(aux_to_str).unwrap() {
     active.kill().ok();
     active = update()?;
   }
